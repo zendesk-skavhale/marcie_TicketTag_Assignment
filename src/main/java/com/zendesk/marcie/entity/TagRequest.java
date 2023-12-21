cae31557ab07f77ca04f9e69aed57ffed45edeef
zendesk-skavhale <shraddha.kavhale@zendesk.com>
2023-12-21 21:01:29 +0530

    added TagRequest class to give requestBody object

---
+++ src/main/java/com/zendesk/marcie/entity/TagRequest.java
@@ -0,0 +1,12 @@
+package com.zendesk.marcie.entity;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class TagRequest {
+    
+    public ArrayList<String> tags;
+    public Date updated_stamp;
+    boolean  safe_update=true;
+}
+
