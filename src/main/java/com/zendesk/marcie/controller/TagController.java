875d0e80d6495c2e119ae105679e9d074bf81f92
zendesk-skavhale <shraddha.kavhale@zendesk.com>
2023-12-21 21:06:07 +0530

    added createTicketTag API in controller

--- src/main/java/com/zendesk/marcie/controller/TagController.java
+++ src/main/java/com/zendesk/marcie/controller/TagController.java
@@ -3,9 +3,13 @@ package com.zendesk.marcie.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.zendesk.marcie.entity.TagRequest;
 import com.zendesk.marcie.service.TagService;
 
 @RestController
@@ -19,4 +23,10 @@ public class TagController {
     public ResponseEntity<String> getTicketData(){
         return tagService.getTicketData();
     }
+    @PostMapping(value="CreateTicketTag/{ticketId}")
+    public TagRequest createTicketTag(@RequestBody TagRequest tagRequest, @PathVariable String ticketId){
+        return tagService.createTagInTicket(tagRequest,ticketId);
+    }
+
+    
 }
