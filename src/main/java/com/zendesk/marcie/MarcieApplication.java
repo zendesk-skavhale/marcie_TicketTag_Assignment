4e07cd1f754d5b2afb13913b3cf9e10e66eb3256
zendesk-skavhale <shraddha.kavhale@zendesk.com>
2023-12-25 10:58:32 +0530

    modified main class to integrate commandLineRunner

--- src/main/java/com/zendesk/marcie/MarcieApplication.java
+++ src/main/java/com/zendesk/marcie/MarcieApplication.java
@@ -1,20 +1,28 @@
 package com.zendesk.marcie;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import com.zendesk.marcie.console_module.TicketManager;
+
 @SpringBootApplication
-public class MarcieApplication {
+public class MarcieApplication implements CommandLineRunner{
+
+	@Autowired
+	TicketManager ticketManager;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MarcieApplication.class, args);
 	}
 
-		@Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+	@Override
+	public void run(String... args) throws Exception {
+		ticketManager.start();
+		
+	}
 
 }
