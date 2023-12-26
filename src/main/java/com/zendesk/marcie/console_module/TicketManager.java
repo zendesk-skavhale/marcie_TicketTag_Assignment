bbb2587ff6eb68b3da06efc8c4c83b14c962e166
zendesk-skavhale <shraddha.kavhale@zendesk.com>
2023-12-26 08:42:04 +0530

    created console_module for managing UI operations

---
+++ src/main/java/com/zendesk/marcie/console_module/TicketManager.java
@@ -0,0 +1,166 @@
+package com.zendesk.marcie.console_module;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.zendesk.marcie.entity.Root;
+import com.zendesk.marcie.entity.TagRequest;
+import com.zendesk.marcie.entity.TagResponse;
+import com.zendesk.marcie.entity.Ticket;
+import com.zendesk.marcie.service.TagService;
+import com.zendesk.marcie.service.TicketService;
+
+@Component
+public class TicketManager {
+
+    private Scanner scanner;
+
+    @Autowired
+    private TagService tagService;
+
+    @Autowired
+    private TicketService ticketService;
+
+    @Autowired
+    private TagRequest tagRequest;
+
+    private List<Ticket> ticketList;
+
+    public TicketManager() {
+        this.ticketList = new ArrayList<>();
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void start() {
+        while (true) {
+            showMenu();
+            System.out.print("Enter Option : ");
+            int choice = scanner.nextInt();
+            handleChoice(choice);
+        }
+    }
+
+    private void handleChoice(int choice) {
+
+        switch (choice) {
+            case 1:
+                String leftAlignFormat = getLeftAlignFormat();
+                try {
+                    // Root root = tagService.getTicketData();
+                    // System.out.println("Ticket List:" +root);
+                    Root obj = ticketService.getTicketData();
+                    // System.out.println(obj);
+                    obj.ticketList.stream().forEach(ticket -> {
+                        System.out.format(leftAlignFormat, ticket.getId(), ticket.getCreated_at(),
+                                ticket.getUpdated_at(), ticket.getTags());
+                    });
+                } catch (Exception e) {
+                    System.out.println("Something went wrong..");
+                    e.printStackTrace();
+                }
+                System.out.println("\n\n\n");
+                // showMenu();
+                break;
+
+            case 2:
+                try {
+                    System.out.println("Enter ticket no in which you want to add Tag");
+                    int ticket_no = scanner.nextInt();
+                    System.out.println("Enter tag to add : ");
+                    String tag = scanner.next();
+                    List<String> list = new ArrayList<>();
+                    list.add(tag);
+
+                    tagRequest.setTags(list);
+                    TagResponse obj = tagService.createTagInTicket(tagRequest, ticket_no);
+                    if (obj != null && null != obj.getTags()) {
+                        Root root = ticketService.getTicketById(ticket_no);
+                        System.out.format(getLeftAlignFormat(), root.getTicket().getId(),
+                                root.getTicket().getCreated_at(),
+                                root.getTicket().getUpdated_at(), root.getTicket().getTags());
+
+                    } else {
+                        System.out.println("Someting went wrong to add tags");
+                    }
+
+                } catch (Exception e) {
+                    System.out.println("Something went wrong..");
+                }
+                System.out.println("\n\n\n");
+                break;
+
+            case 3:
+                System.out.println("Enter ticket no in which you want to delete Tag");
+                int ticket_no = scanner.nextInt();
+                System.out.println("Enter tag to delete : ");
+                String tag = scanner.next();
+                Ticket ticket = new Ticket();
+                List<String> list = new ArrayList<>();
+                list.add(tag);
+
+                ticket.setTags(list);
+
+                tagService.deleteTagsFromTicket(ticket, ticket_no);
+                System.out.format(getDelAlignFormat(), ticket_no,
+                        ticket.getTags());
+                System.out.println();
+
+                break;
+
+            case 4:
+
+                System.out.println("\nClosing application...");
+                System.exit(0);
+
+                break;
+
+            default:
+                break;
+        }
+
+    }
+
+    private String getDelAlignFormat() {
+        String leftAlignFormat = "| %-10s | %-30s %n";
+        System.out.println("\n\n\n");
+        System.out.format(
+                "+------------+--------------------+%n");
+        System.out.format(
+                "| Ticket_No  |   Tags              %n");
+        System.out.format(
+                "+------------+--------------------+%n");
+        return leftAlignFormat;
+    }
+
+    private String getLeftAlignFormat() {
+        String leftAlignFormat = "| %-10s | %-30s | %-30s | %-70s |%n";
+        System.out.println("\n\n\n");
+        System.out.format(
+                "+------------+--------------------------------+------------+------------+--------------------------------------------------------------+%n");
+        System.out.format(
+                "| Ticket_No         | Created at                    |  Updated_at       |   Tags                                                       |%n");
+        System.out.format(
+                "+------------+--------------------------------+------------+------------+--------------------------------------------------------------+%n");
+        // System.out.format(leftAlignFormat, "1", "Show All Ticket","","","");
+        return leftAlignFormat;
+    }
+
+    private void showMenu() {
+        String leftAlignFormat = "| %-10s | %-30s |%n";
+
+        System.out.format("+------------+--------------------------------+%n");
+        System.out.format("| Option     | Description                    |%n");
+        System.out.format("+------------+--------------------------------+%n");
+        System.out.format(leftAlignFormat, "1", "Show All Ticket");
+        System.out.format(leftAlignFormat, "2", "Add Ticket");
+        System.out.format(leftAlignFormat, "3", "Delete Ticket");
+        System.out.format(leftAlignFormat, "4", "Exit");
+        System.out.format("+------------+--------------------------------+%n");
+        ;
+    }
+
+}
