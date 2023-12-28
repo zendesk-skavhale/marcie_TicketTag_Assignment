4277901f6470a899b5ffe2c43213315dad86fabf
zendesk-skavhale <shraddha.kavhale@zendesk.com>
2023-12-28 12:38:54 +0530

    Added negative scanarios to unit test cases

--- src/test/java/com/zendesk/marcie/TagServiceTest.java
+++ src/test/java/com/zendesk/marcie/TagServiceTest.java
@@ -1,6 +1,10 @@
 package com.zendesk.marcie;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
@@ -8,91 +12,128 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.client.RestTemplate;
-
 
 import com.zendesk.marcie.entity.TagRequest;
 import com.zendesk.marcie.entity.TagResponse;
 import com.zendesk.marcie.entity.Ticket;
 import com.zendesk.marcie.service.TagService;
 
-
-
-@ExtendWith(MockitoExtension.class) 
+@ExtendWith(MockitoExtension.class)
 public class TagServiceTest {
-
 
     @Mock
     private RestTemplate restTemplate;
 
-    @InjectMocks 
+    @InjectMocks
     private TagService tagService;
 
     @Captor
     private ArgumentCaptor<HttpEntity<TagRequest>> requestEntityCaptor;
 
-    private String baseUrl = "https://z3nzendeskcodingchallenge6305.zendesk.com/api/v2/tickets/";
+    @Value("${zendesk.subdomain}")
+    private String baseUrl;
 
-    @BeforeEach  
-     public void setup() {
-    ReflectionTestUtils.setField(tagService, "baseUrl", "https://z3nzendeskcodingchallenge6305.zendesk.com/api/v2/tickets/");
-}
+    @Value("${api.username}")
+    private String username;
+
+    @Value("${api.password}")
+    private String password;
+
+    @BeforeEach
+    public void setup() {
+        ReflectionTestUtils.setField(tagService, "baseUrl", "");
+    }
+
+    @Test
+    public void testCreateTagInTicket_Success() {
+        // Given
+        int ticketId = 1;
+        TagRequest tagRequest = new TagRequest();
+        TagResponse tagResponse = new TagResponse();
+
+        when(restTemplate.exchange(
+                anyString(), // match any URL
+                eq(HttpMethod.PUT), // match PUT method precisely
+                any(HttpEntity.class), // match any HttpEntity
+                eq(TagResponse.class))) // match TagResponse.class precisely
+                .thenReturn(new ResponseEntity<>(tagResponse, HttpStatus.OK));
+
+        // No exceptions should be thrown
+        assertDoesNotThrow(() -> {
+            TagResponse returnedTagResponse = tagService.createTagInTicket(tagRequest, ticketId);
+            assertEquals(tagResponse, returnedTagResponse);
+        });
+    }
 
     @Test
     public void testCreateTagInTicket() {
-        // Given
         int ticketId = 1;
-        TagRequest tagRequest = new TagRequest();   
-        TagResponse tagResponse = new TagResponse();   
+        TagRequest tagRequest = new TagRequest();
+        TagResponse tagResponse = new TagResponse();
 
-        HttpHeaders headers = new HttpHeaders();  
-        HttpEntity<TagRequest> requestEntity = new HttpEntity<>(tagRequest, headers);  
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<TagRequest> requestEntity = new HttpEntity<>(tagRequest, headers);
 
-        when(restTemplate.exchange(   
-                eq(baseUrl + ticketId + "/tags"),  
-                eq(HttpMethod.PUT),  
-                requestEntityCaptor.capture(),  
-                eq(TagResponse.class))  
-        ).thenReturn(new ResponseEntity<>(tagResponse, HttpStatus.OK));
+        when(restTemplate.exchange(
+                eq(baseUrl + ticketId + "/tags"),
+                eq(HttpMethod.PUT),
+                eq(requestEntity),
+                eq(TagResponse.class))).thenReturn(new ResponseEntity<>(tagResponse, HttpStatus.BAD_REQUEST));
 
-        TagResponse result = tagService.createTagInTicket(tagRequest, ticketId);
-
-        assertEquals(tagResponse, result); 
-    
-        assertEquals(tagRequest, requestEntityCaptor.getValue().getBody());
+        assertThrows(RuntimeException.class, () -> {
+            tagService.createTagInTicket(tagRequest, ticketId);
+        });
     }
 
-    @Test 
-    public void testDeleteTagsFromTicket() {
+    @Test
+    public void testDeleteTagsFromTicket_Success() {
         // Given
         int ticketId = 123;
         Ticket ticket = new Ticket();
         TagResponse tagResponse = new TagResponse();
-        
-        HttpHeaders headers = new HttpHeaders(); // Create headers
+
+        // Mock the restTemplate to return OK status
+        when(restTemplate.exchange(
+                anyString(),
+                any(HttpMethod.class),
+                any(HttpEntity.class),
+                ArgumentMatchers.<Class<TagResponse>>any()))
+                .thenReturn(new ResponseEntity<>(tagResponse, HttpStatus.OK));
+
+        // No exceptions should be thrown
+        assertDoesNotThrow(() -> {
+            TagResponse returnedTagResponse = tagService.deleteTagsFromTicket(ticket, ticketId);
+            assertEquals(tagResponse, returnedTagResponse);
+        });
+    }
+
+    @Test
+    public void testDeleteTagsFromTicket() {
+        int ticketId = 123;
+        Ticket ticket = new Ticket();
+        TagResponse tagResponse = new TagResponse();
+
+        HttpHeaders headers = new HttpHeaders();
         HttpEntity<Ticket> requestEntity = new HttpEntity<>(ticket, headers);
 
         when(restTemplate.exchange(
                 eq(baseUrl + ticketId + "/tags"),
                 eq(HttpMethod.DELETE),
-                requestEntityCaptor.capture(),
-                eq(TagResponse.class))
-        ).thenReturn(new ResponseEntity<>(tagResponse, HttpStatus.OK));
+                eq(requestEntity),
+                eq(TagResponse.class))).thenReturn(new ResponseEntity<>(tagResponse, HttpStatus.BAD_REQUEST));
 
-        TagResponse result = tagService.deleteTagsFromTicket(ticket, ticketId);
-
-        assertEquals(tagResponse, result); 
-    
-        assertEquals(ticket, requestEntityCaptor.getValue().getBody());
+        assertThrows(RuntimeException.class, () -> {
+            tagService.deleteTagsFromTicket(ticket, ticketId);
+        });
     }
 
-   
 }
-
-
--- src/test/java/com/zendesk/marcie/TicketServiceTest.java
+++ src/test/java/com/zendesk/marcie/TicketServiceTest.java
@@ -1,6 +1,9 @@
 package com.zendesk.marcie;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
@@ -13,6 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import com.zendesk.marcie.entity.Root;
@@ -28,7 +32,9 @@ public class TicketServiceTest {
     @InjectMocks
     private TicketService ticketService;
 
-    private String baseUrl = "https://z3nzendeskcodingchallenge6305.zendesk.com/api/v2/tickets/";
+    @Value("${zendesk.subdomain}")
+    private String baseUrl;
+
     @Value("${api.username}")
     private String username;
 
@@ -38,54 +44,78 @@ public class TicketServiceTest {
     @BeforeEach
     public void setup() {
         ReflectionTestUtils.setField(ticketService, "baseUrl",
-                "https://z3nzendeskcodingchallenge6305.zendesk.com/api/v2/tickets/");
+                "");
     }
 
     @Test
     public void testGetTicketData() throws NoDataAvailableException {
-        // Given
-        Root root = new Root();
-        // set your root's fields here...
-        HttpHeaders headers = new HttpHeaders(); // Create headers
-        headers = ticketService.createHeaders(username, password);
-        HttpEntity<String> requestEntity = new HttpEntity<>("parameters", headers);
-        // Mock restTemplate's exchange method
-        when(restTemplate.exchange(
-                eq(baseUrl),
-                eq(HttpMethod.GET),
-                eq(requestEntity),
-                eq(Root.class))).thenReturn(new ResponseEntity<>(root, HttpStatus.OK));
+        Root mockRoot = new Root();
 
-        // When
+        ResponseEntity<Root> mockResponse = new ResponseEntity<>(mockRoot, HttpStatus.OK);
+
+        when(restTemplate.exchange(
+                anyString(),
+                eq(HttpMethod.GET),
+                any(HttpEntity.class),
+                eq(Root.class))).thenReturn(mockResponse);
+
         Root result = ticketService.getTicketData();
 
-        // Then
-        assertEquals(root, result); // Verify that returned Root object is as expected
+        assertEquals(mockRoot, result);
     }
 
     @Test
-    public void testGetTicketById() {
-        // Given
+    public void testGetTicketById() throws NoDataAvailableException {
         int ticketId = 1;
         Root root = new Root();
-        // set your root's fields here...
-        HttpHeaders headers = new HttpHeaders(); // Create headers
-        headers = ticketService.createHeaders(username, password);
 
-        HttpEntity<String> requestEntity = new HttpEntity<>("parameters", headers);
-
-        // Mock the restTemplate exchange method
         when(restTemplate.exchange(
-                eq(baseUrl + ticketId),
+                anyString(),
                 eq(HttpMethod.GET),
-                eq(requestEntity),
+                any(HttpEntity.class),
                 eq(Root.class))).thenReturn(new ResponseEntity<>(root, HttpStatus.OK));
 
-       
         Root result = ticketService.getTicketById(ticketId);
 
-       
-        assertEquals(root, result); 
+        assertEquals(root, result);
+    }
+
+    @Test
+    public void testGetTicketDataNegative() throws NoDataAvailableException {
+        Root mockRoot = new Root();
+
+        ResponseEntity<Root> mockResponse = new ResponseEntity<>(mockRoot, HttpStatus.BAD_REQUEST);
+
+        when(restTemplate.exchange(
+                anyString(),
+                eq(HttpMethod.GET),
+                any(HttpEntity.class),
+                eq(Root.class))).thenReturn(mockResponse);
+
+        assertThrows(NoDataAvailableException.class, () -> {
+            ticketService.getTicketData();
+        });
+    }
+
+    @Test
+    public void testGetTicketByIdNegative() {
+        int ticketId = 1;
+        String username = "testUsername";
+        String password = "testPassword";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBasicAuth(username, password);
+
+        when(restTemplate.exchange(
+                anyString(),
+                any(),
+                any(),
+                eq(Root.class)))
+                .thenThrow(new RestClientException("Error during exchange"));
+
+        assertThrows(NoDataAvailableException.class, () -> {
+            ticketService.getTicketById(ticketId);
+        });
     }
 
 }
