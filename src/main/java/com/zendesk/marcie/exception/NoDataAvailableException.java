4b2d184a1e4f57a43448197bc8cbd29c75eae7be
zendesk-skavhale <shraddha.kavhale@zendesk.com>
2023-12-28 12:36:53 +0530

    Added parameterized constructor

--- src/main/java/com/zendesk/marcie/exception/NoDataAvailableException.java
+++ src/main/java/com/zendesk/marcie/exception/NoDataAvailableException.java
@@ -1,8 +1,13 @@
 package com.zendesk.marcie.exception;
 
-public class NoDataAvailableException extends Exception  {
-    
+import org.springframework.web.client.RestClientException;
+
+public class NoDataAvailableException extends Exception {
+
     public NoDataAvailableException(String message) {
         super(message);
+    }
+
+    public NoDataAvailableException(String string, RestClientException ex) {
     }
 }
